4d6a5016ba2e50343156a364eef7f8d9611b125b
Sreechandh <sdevire3@asu.edu>
2025-02-07 22:20:19 -0700

    server update

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const connectDB = require("./config/db");
+const chatSocket = require("./sockets/chatSocket");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/clothing", require("./routes/clothingRoutes"));
+app.use("/api/match", require("./routes/matchRoutes"));
+app.use("/api/chat", require("./routes/chatRoutes"));
+app.use("/api/payment", require("./routes/paymentRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+chatSocket(server);
